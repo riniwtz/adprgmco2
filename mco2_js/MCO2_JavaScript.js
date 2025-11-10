4adda92c29dc4a4f9cf550ec0a100457e8c0917e
ejcunanan <ejcunanan8@gmail.com>
2025-11-10 22:37:17 +0800

    Update MCO2_JavaScript.js

--- mco2_js/MCO2_JavaScript.js
+++ mco2_js/MCO2_JavaScript.js
@@ -17,6 +17,29 @@ let records = [];
 
 const filePath = 'dpwh_flood_control_projects.csv';
 
+const regionIslandGroup = {
+  "National Capital Region": "Luzon",
+  "Cordillera Administrative Region": "Luzon",
+
+  "Region I": "Luzon",
+  "Region II": "Luzon",
+  "Region III": "Luzon",
+  "Region IV-A": "Luzon",
+  "Region IV-B": "Luzon",
+  "Region V": "Luzon",
+
+  "Region VI": "Visayas",
+  "Region VII": "Visayas",
+  "Region VIII": "Visayas",
+
+  "Region IX": "Mindanao",
+  "Region X": "Mindanao",
+  "Region XI": "Mindanao",
+  "Region XII": "Mindanao",
+  "Region XIII": "Mindanao",
+
+  "BARMM": "Mindanao"
+};
 function loadFileSync() {
   const fileContent = fs.readFileSync(filePath, 'utf8');
 
@@ -61,7 +84,6 @@ function saveToCSV(records, outputFileName) {
   fs.writeFileSync(outputFileName, csvOutput, 'utf8');
   console.log(`CSV written immediately to ${outputFileName}`);
 }
-
 function generateCostSavings(ApprovedBudgetForContract, ContractCost){
   return ApprovedBudgetForContract - ContractCost;
 }
@@ -69,41 +91,7 @@ function generateCostSavings(ApprovedBudgetForContract, ContractCost){
 function generateCompletionDelayDays(sDate, aDate){
   const msPerDay = 1000 * 60 * 60 * 24;
   return  Math.round((aDate - sDate) / msPerDay);
-}
-
-function costSavingsMedian(dataSet){
-  let median = 0;
-  let dataSize = dataSet.length;
-  if(dataSize % 2 === 0){
-    median = (dataSet[dataSize/2] + dataSet[dataSize/2+1])/2
-  }else{
-    median = dataSet[Math.floor(dataSize/2)];
   }
-  return median;
-}
-
-function AverageDelayDays(dataSet){
-  let Average = 0;
-  let size = dataSet.length;
-  let sum = 0;
-  for(let i = 0; i < size; i++){
-    sum += dataSet[i];
-  }
-  Average = sum/size;
-  return Average;
-}
-
-function PercentageDelay(dataSet){
-  let percentage = 0;
-  let RegionSize = dataSet.length;
-  for(let i = 0; i < RegionSize; i++){
-
-  }
-}
-
-function EfficiencyScore(medianCostSavings, AverageDelay){
-  return (medianCostSavings/AverageDelay) * 100;
-}
 
 function report1(){
   console.log("Outputs are saved into individual CSV Report Files.");
@@ -129,6 +117,7 @@ function report1(){
   const uniqueRegions = Array.from(new Set(records.map(r => r.Region)));
   const report1 = uniqueRegions.map(region => ({
     Region: region,
+    MainIsland: regionIslandGroup[region],
     TotalApprovedBudget: 0,
     MedianCostSavings: 0,
     AverageCompletionDelayDays: 0,
@@ -167,26 +156,105 @@ function report1(){
     delayDays[data.Region].push(data.CompletionDelayDays);
   });
 
+  //calculate average completion delay days for each region
   report1.forEach(data =>{
     data.AverageCompletionDelayDays = AverageDelayDays(delayDays[data.Region]);
   });
   
+  //calculate percentage delay for each region
   report1.forEach(data =>{
-
     data.PercentageDelay = PercentageDelay(delayDays[data.Region]);
   })
 
+  //calculate efficiency score for each region
   report1.forEach(data =>{
     data.EfficiencyScore = EfficiencyScore(data.MedianCostSavings, data.AverageCompletionDelayDays);
   });
-  
-  
 
-  //average completion delay days and percentage delay for each region
-  saveToCSV(template,"template.csv");
+  report1.sort((a, b) => {
+    return b.EfficiencyScore - a.EfficiencyScore;
+  });
   
   saveToCSV(report1, "report1s.csv");
+
+  
+
+  
+
+  function costSavingsMedian(dataSet){
+    let median = 0;
+    let dataSize = dataSet.length;
+    if(dataSize % 2 === 0){
+      median = (dataSet[dataSize/2] + dataSet[dataSize/2+1])/2
+    }else{
+      median = dataSet[Math.floor(dataSize/2)];
+    }
+    return median;
+  }
+
+  function AverageDelayDays(dataSet){
+    let Average = 0;
+    let size = dataSet.length;
+    let sum = 0;
+    for(let i = 0; i < size; i++){
+      sum += dataSet[i];
+    }
+    Average = sum/size;
+    return Average;
+  }
+
+  function PercentageDelay(dataSet){
+    let percentage = 0;
+    let counter = 0;
+    let RegionSize = dataSet.length;
+    for(let i = 0; i < RegionSize; i++){
+      if(dataSet[i] > 30){
+        counter++;
+      }
+    }
+    return (counter/RegionSize) * 100;
+  }
+
+  function EfficiencyScore(medianCostSavings, AverageDelay){
+    return (medianCostSavings/AverageDelay) * 100;
+  }
 }
+
+
+function report2(){
+
+  const uniqueContractors = Array.from(new Set(records.map(r => r.Contractor)));
+  const report2 = uniqueContractors.map(contractor => ({
+    Contractor: contractor,
+    TotalProjects: 0,
+    AverageCompletionDelayDays: 0,
+    TotalCostSavings:0,
+    ReliabilityIndex:0
+  }));
+
+  let template2 = records.map(record => {
+    return {
+      Contractor: record.Contractor,
+      apbudg: record.ApprovedBudgetForContract,
+      cost: record.ContractCost,
+      sdate: record.StartDate,
+      adate: record.ActualCompletionDate,
+      CostSavings: generateCostSavings(record.ApprovedBudgetForContract, record.ContractCost),
+      CompletionDelayDays: generateCompletionDelayDays(record.StartDate, record.ActualCompletionDate)
+    };
+  });
+
+
+  saveToCSV(template2, "template2.csv");
+}
+
+
+
+
+
+
+
+
 //MAIN
 while (running) {
   console.log("Select Language Implementation:");
@@ -214,6 +282,7 @@ while (running) {
         console.log("No file loaded yet.");
       } else {
         report1();
+        report2();
         
       }
       break;
